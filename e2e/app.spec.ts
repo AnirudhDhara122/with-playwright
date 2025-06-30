1c71c66c2bee066e54f367c4e5e33363b92f0120
Anirudhdhara1997 <venkatanagasaiswamyanirudhdhar@gmail.com>
2025-06-30 08:17:26 -0500

    New Changes

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -10,3 +10,13 @@ test("should navigate to the about page", async ({ page }) => {
   // The new page should contain an h1 with "About"
   // await expect(page.locator("h1")).toContainText("About");
 });
+
+test("should test do something", async ({ page }) => {
+  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+  await page.goto("/");
+  // Find an element with the text 'About' and click on it
+  await page.click("text=Do Something");
+  // The new URL should be "/about" (baseURL is used there)
+  // The new page should contain an h1 with "About"
+  await expect(page.locator("p")).toContainText("Something was done");
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+"use client"
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Page() {
+    const [done, setIsDone] = useState<boolean>(false)
+    const doSomething = () => {
+        setIsDone(true)
+    }
     return (
         <div>
             <h1>Hello, Next.js!</h1>
+            <button onClick={doSomething}>
+                Do Something
+            </button>
+            <p>
+                {done && `Something was done`}
+            </p>
             <Link href="/about">about</Link>
         </div>
     )
